01b5e7000eaea423decbb7cd4252e40e4d945896
galbar3 <galbaryosef@mail.tau.ac.il>
2023-09-26 17:18:30 +0300

    added functions

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
+/* 
+Express is a minimal and flexible Node.js web application framework that provides a robust set
+of features for building web and mobile applications
+*/
 const express = require('express');
 const cors = require('cors');
+/* 
+The path module in node js is used for managing and altering file paths.
+It's a part of the core modules with methods that help you deal with directory
+and file path names on the local machine's file system
+*/
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 /*
 CORS (Cross-Origin Resource Sharing) is a browser security feature that restricts
@@ -16,6 +25,25 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/api/helloworld', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/api/createuser', (req, res) => {
+  
+  let time = new Date();
+
+  res.send("POST Request accepted , user created succussfully , Called at: " + time.toDateString());
+});
+
+
+app.get('/api/time', (req, res) => {
+  
+  let time = new Date();
+
+  res.send(time.toDateString());
+});
+
+app.get('/api/name', (req, res) => {
+  res.send('gal');
 });
 
 app.get('/*', (req, res) => {
